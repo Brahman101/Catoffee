fdee9b49bce91aff7a81bbef5434e3eb575e0902
Brahman101 <127399156+Brahman101@users.noreply.github.com>
2024-10-06 13:18:02 +0700

    progress kemarin sih

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="{{ asset('css/components/footer.css') }}">
+<footer>
+    <p>Hak Cipta © 2024 Cattofee. Hak Cipta dilindungi oleh undang-undang. Dilarang mengambil foto dan tulisan tanpa
+        izin dari Cattofee.</p>
+    <div class="social-icons">
+        <i class="fa-brands fa-x-twitter" href="/"></i>
+        <i class="fa-brands fa-instagram" href="/"></i>
+        <i class="fa-brands fa-youtube" href="/"></i>
+    </div>
+</footer>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="{{ asset('css/components/navbar.css') }}">
+<nav>
+    <div class="nav-logo">
+        <img src="{{ asset('images/Cattofee.png') }}" alt="Catofeee Logo">
+    </div>
+    <ul>
+        <li>
+            <a href="/" class="{{ Request::is('/') ? 'active' : '' }}">Home</a>
+        </li>
+        <li>
+            <a href="/menu" class="{{ Request::is('menu') ? 'active' : '' }}">Menu</a>
+        </li>
+        <li>
+            <a href="/cats" class="{{ Request::is('cats') ? 'active' : '' }}">The Cats</a>
+        </li>
+        <li>
+            <a href="/contact" class="{{ Request::is('contact') ? 'active' : '' }}">Contact</a>
+        </li>
+        <li>
+            <a href="/about" class="{{ Request::is('about') ? 'active' : '' }}">About Us</a>
+        </li>
+    </ul>
+
+    <div class="nav-actions">
+        @guest
+            <a href="/login" class="login-btn">Masuk</a>
+            <a href="/register" class="register-btn">Daftar</a>
+        @else
+            <div class="left-icons">
+                <i class="fa-solid fa-cart-shopping"></i>
+            </div>
+        @endguest
+    </div>
+</nav>
